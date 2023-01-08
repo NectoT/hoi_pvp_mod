65186ce696d9c68772748c03d41d80230eaa1542
NectoT <nectot2@gmail.com>
2023-01-08 21:46:49 +0300

    fix states

--- scripts/state_edit.py
+++ scripts/state_edit.py
@@ -11,14 +11,14 @@ victory_points = {
     839: 15, 848: 15, 629: 20, 630: 20, 649: 5, 652: 5, 610: 1, 592: 1, 591: 1, 590: 1, 508: 1,
     509: 1, 841: 10, 846: 10, 842: 1, 845: 1, 445: 5, 446: 5, 696: 10, 703: 10, 830: 3, 825: 3,
     720: 3, 712: 3, 632: 1, 633: 1, 897: 1, 898: 1, 930: 20, 931: 20, 963: 50, 964: 50, 1042: 15,
-    1045: 15, 1001: 5, 1002: 5, 1035: 3, 1034: 3, 1016: 1, 998: 1, 1015: 1, 997: 1, 1097: 1, 
+    1045: 15, 1001: 5, 1002: 5, 1035: 3, 1034: 3, 1016: 1, 998: 1, 1015: 1, 997: 1, 1097: 1,
     1058: 1, 1098: 1, 1059: 1
 }
 
 countries = {
     'EVA': [
-        31, 37, 39, 67, 71, 33, 69, 35, 48, 75, 73, 77, 87, 89, 57, 59, 99, 55, 79, 91, 93, 
-        51, 53, 63, 65, 61, 49, 97, 82, 81, 84, 27, 28, 29, 3, 5, 85, 95, 43, 44, 45
+        31, 37, 39, 67, 71, 33, 69, 35, 48, 75, 73, 77, 87, 89, 57, 59, 99, 55, 79, 91, 93,
+        51, 53, 63, 65, 61, 49, 97, 82, 81, 84, 27, 28, 29, 3, 5, 85, 95, 43, 44, 45, 102
     ],
     'WVA': [
         64, 52, 94, 96, 101, 83, 60, 62, 50, 92, 100, 98, 90, 78, 80, 86, 54, 42, 68, 88, 78,
@@ -40,7 +40,7 @@ for filename in listdir(state_path):
     state = parse_file(state_path + "/" + filename, filename)
     if 'state' not in state or 'provinces' not in state['state']:
         continue
-    
+
     state['state']['history'] = {}
     for country in countries:
         if int(state['state']['id']) in countries[country]:
@@ -53,10 +53,10 @@ for filename in listdir(state_path):
             state['state']['history'][vp_string] = {}
             state['state']['history'][vp_string][province] = None
             state['state']['history'][vp_string][victory_points[int(province)]] = None
-    
+
     if int(state['state']['id']) in impassable_states:
         state['state']['impassable'] = 'yes'
-    
+
     for tag in cores:
         if int(state['state']['id']) in cores[tag]:
             state['state']['history']['add_core_of'] = tag
@@ -241,7 +241,7 @@ for id in [42, 87]:
         'arms_factory': 3
     })
 
-for id in [72, 73, 74, 75, 76, 77, 37, 38]:
+for id in [72, 73, 74, 75, 76, 77, 38]:
     set_buildings(states[id], {
         'infrastructure': 3,
     })
@@ -325,27 +325,27 @@ set_buildings(states[25], {
     'industrial_complex': 1,
     279: {
         'naval_base': 3
-    } 
+    }
 })
 set_buildings(states[28], {
    'infrastructure': 1,
     'industrial_complex': 1,
     280: {
         'naval_base': 3
-    } 
+    }
 })
 
 set_buildings(states[4], {
    'infrastructure': 1,
     1136: {
         'naval_base': 3
-    } 
+    }
 })
 set_buildings(states[5], {
    'infrastructure': 1,
     1137: {
         'naval_base': 3
-    } 
+    }
 })
 
 set_buildings(states[6], {
@@ -382,8 +382,8 @@ set_buildings(states[19], {
 
 set_buildings(states[20], {
     'infrastructure': 4,
-    'industrial_complex': 1,
-    'arms_factory': 3,
+    'industrial_complex': 2,
+    'arms_factory': 2,
     'dockyard': 3,
     953: {
         'naval_base': 5
@@ -391,8 +391,8 @@ set_buildings(states[20], {
 })
 set_buildings(states[21], {
     'infrastructure': 4,
-    'industrial_complex': 1,
-    'arms_factory': 3,
+    'industrial_complex': 2,
+    'arms_factory': 2,
     'dockyard': 3,
     954: {
         'naval_base': 5
